34973abb72fc4ed290667a8b581e766530475d11
davidcoats <david@getsafetycone.com>
2023-02-23 14:41:31 -0800

    Initial commit.

---
+++ source/R5T.T0161/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0161
+{
+	/// <summary>
+	/// Strongly-typed code element name types. (Type name, method name, etc.)
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
