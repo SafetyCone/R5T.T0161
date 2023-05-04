0a698480a9c989ae092eb57e69d9dbb19b59f9da
davidcoats <david@getsafetycone.com>
2023-05-04 00:28:12 -0700

    Interim changes.

--- source/R5T.T0161/Code/Strong Types/Interfaces/IMethodName.cs
+++ source/R5T.T0161/Code/Strong Types/Interfaces/IMethodName.cs
@@ -7,6 +7,9 @@ namespace R5T.T0161
 {
     /// <summary>
     /// Chooses <see cref="ISimpleMethodName"/> as the default for method names.
+    /// <para>
+    /// <inheritdoc cref="ISimpleMethodName" path="/summary"/>
+    /// </para>
     /// </summary>
     [StrongTypeMarker]
     public interface IMethodName : IStrongTypeMarker,
